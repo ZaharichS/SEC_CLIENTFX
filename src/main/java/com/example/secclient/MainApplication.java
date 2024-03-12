cc2ffa930c5a59adf0c1566413feed26aec7b487
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-03-12 15:23:06 +0300

    Fixed (chechk stage.getIcons...)

--- src/main/java/com/example/secclient/MainApplication.java
+++ src/main/java/com/example/secclient/MainApplication.java
@@ -3,17 +3,21 @@ package com.example.secclient;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 689, 459);
+        stage.setTitle("Your Book App");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Objects.class.getResourceAsStream("images/favicon-32x32.png"))));
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/com/example/secclient/MainController.java
+++ src/main/java/com/example/secclient/MainController.java
@@ -1,14 +1,63 @@
 package com.example.secclient;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<?, ?> bookAuthor;
+
+    @FXML
+    private TableColumn<?, ?> bookGenre;
+
+    @FXML
+    private TableColumn<?, ?> bookId;
+
+    @FXML
+    private TableColumn<?, ?> bookPublish;
+
+    @FXML
+    private TableView<?> bookTable;
+
+    @FXML
+    private TableColumn<?, ?> bookTitle;
+
+    @FXML
+    void addBookAction(ActionEvent event) {
+
     }
+
+    @FXML
+    void addOrChangeAuthorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeCityAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeGenreAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangePublisherAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeBookAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBookAction(ActionEvent event) {
+
+    }
+
 }
