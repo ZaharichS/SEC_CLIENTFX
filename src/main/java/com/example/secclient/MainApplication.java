2b29b1e56e8aa1f69db3a847a48c9cf008ab266b
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-03-13 21:11:43 +0300

    Fixed ui

--- src/main/java/com/example/secclient/MainApplication.java
+++ src/main/java/com/example/secclient/MainApplication.java
@@ -13,11 +13,10 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 689, 380);
+        Scene scene = new Scene(fxmlLoader.load(), 840, 465);
         stage.setTitle("Your Book App");
         /*
         Устанавливаем иконку на родном toolbar
-        Подключаем к главному окну css-стиль
         Параметр который запрещает масштабировать окно
         */
         stage.getIcons().add(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("image/main_icon.png"))));
